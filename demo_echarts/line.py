9fd92bd8867fc71097413830ae00d14cd8d6993b
EmptinessOfNature <148034676+EmptinessOfNature@users.noreply.github.com>
2023-10-16 21:23:21 +0800

    Update line.py

--- demo_echarts/line.py
+++ demo_echarts/line.py
@@ -218,6 +218,75 @@ def render_line_race():
     }
     st_echarts(options=option, height="600px")
 
+def gupiao_line_race():
+    # 股票linerace的代码
+    with open("./data/life-expectancy-table.json") as f:
+        raw_data = json.load(f)
+    countries = [
+        "Finland",
+        "France",
+        "Germany",
+        "Iceland",
+        "Norway",
+        "Poland",
+        "Russia",
+        "United Kingdom",
+    ]
+
+    datasetWithFilters = [
+        {
+            "id": f"dataset_{country}",
+            "fromDatasetId": "dataset_raw",
+            "transform": {
+                "type": "filter",
+                "config": {
+                    "and": [
+                        {"dimension": "Year", "gte": 1950},
+                        {"dimension": "Country", "=": country},
+                    ]
+                },
+            },
+        }
+        for country in countries
+    ]
+
+    seriesList = [
+        {
+            "type": "line",
+            "datasetId": f"dataset_{country}",
+            "showSymbol": False,
+            "name": country,
+            "endLabel": {
+                "show": True,
+                "formatter": JsCode(
+                    "function (params) { return params.value[3] + ': ' + params.value[0];}"
+                ).js_code,
+            },
+            "labelLayout": {"moveOverlap": "shiftY"},
+            "emphasis": {"focus": "series"},
+            "encode": {
+                "x": "Year",
+                "y": "Income",
+                "label": ["Country", "Income"],
+                "itemName": "Year",
+                "tooltip": ["Income"],
+            },
+        }
+        for country in countries
+    ]
+
+    option = {
+        "animationDuration": 10000,
+        "dataset": [{"id": "dataset_raw", "source": raw_data}] + datasetWithFilters,
+        "title": {"text": "Income in Europe since 1950"},
+        "tooltip": {"order": "valueDesc", "trigger": "axis"},
+        "xAxis": {"type": "category", "nameLocation": "middle"},
+        "yAxis": {"name": "Income"},
+        "grid": {"right": 140},
+        "series": seriesList,
+    }
+    st_echarts(options=option, height="600px")
+
 
 ST_LINE_DEMOS = {
     "Line: Basic Line Chart": (
@@ -238,6 +307,10 @@ ST_LINE_DEMOS = {
     ),
     "Line: Line Race": (
         render_line_race,
+        "https://echarts.apache.org/examples/en/editor.html?c=line-race",
+    ),
+        "股票: Line Race": (
+        gupiao_line_race,
         "https://echarts.apache.org/examples/en/editor.html?c=line-race",
     ),
 }
