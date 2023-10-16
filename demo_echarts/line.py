0f7e9f63958f8acc494db798efa2ff517549ccd5
EmptinessOfNature <148034676+EmptinessOfNature@users.noreply.github.com>
2023-10-16 22:05:05 +0800

    Update line.py

--- demo_echarts/line.py
+++ demo_echarts/line.py
@@ -212,72 +212,72 @@ def gupiao_line_race():
     }
     st_echarts(options=option, height="600px")
 
-# def render_line_race():
-#     with open("./data/105qqq.json") as f:
-#         raw_data = json.load(f)
-#     countries = [
-#         "France",
-#         "Germany",
-#         "Iceland",
-#         "Norway",
-#         "Poland",
-#         "Russia",
-#         "United Kingdom",
-#     ]
+def render_line_race(): 
+    with open("./data/105qqq.json") as f:
+        raw_data = json.load(f)
+    countries = [
+        "France",
+        "Germany",
+        "Iceland",
+        "Norway",
+        "Poland",
+        "Russia",
+        "United Kingdom",
+    ]
 
-#     datasetWithFilters = [
-#         {
-#             "id": f"dataset_{country}",
-#             "fromDatasetId": "dataset_raw",
-#             "transform": {
-#                 "type": "filter",
-#                 "config": {
-#                     "and": [
-#                         # {"dimension": "Year", "gte": 1950},
-#                         {"dimension": "Code", "=": country},
-#                     ]
-#                 },
-#             },
-#         }
-#         for country in countries
-#     ]
+    datasetWithFilters = [
+        {
+            "id": f"dataset_{country}",
+            "fromDatasetId": "dataset_raw",
+            "transform": {
+                "type": "filter",
+                "config": {
+                    "and": [
+                        # {"dimension": "Year", "gte": 1950},
+                        {"dimension": "Code", "=": country},
+                    ]
+                },
+            },
+        }
+        for country in countries
+    ]
 
-#     seriesList = [
-#         {
-#             "type": "line",
-#             "datasetId": f"dataset_{country}",
-#             "showSymbol": False,
-#             "name": country,
-#             "endLabel": {
-#                 "show": True,
-#                 "formatter": JsCode(
-#                     "function (params) { return params.value[-1] + ': ' + params.value[2];}"
-#                 ).js_code,
-#             },
-#             "labelLayout": {"moveOverlap": "shiftY"},
-#             "emphasis": {"focus": "series"},
-#             "encode": {
-#                 "x": "Year",
-#                 "y": "Income",
-#                 "label": ["Country", "Income"],
-#                 "itemName": "Year",
-#                 "tooltip": ["Income"],
-#             },
-#         }
-#         for country in countries
-#     ]
+    seriesList = [
+        {
+            "type": "line",
+            "datasetId": f"dataset_{country}",
+            "showSymbol": False,
+            "name": country,
+            "endLabel": {
+                "show": True,
+                "formatter": JsCode(
+                    "function (params) { return params.value[-1] + ': ' + params.value[2];}"
+                ).js_code,
+            },
+            "labelLayout": {"moveOverlap": "shiftY"},
+            "emphasis": {"focus": "series"},
+            "encode": {
+                "x": "Year",
+                "y": "Income",
+                "label": ["Country", "Income"],
+                "itemName": "Year",
+                "tooltip": ["Income"],
+            },
+        }
+        for country in countries
+    ]
 
-#     option = {
-#         "animationDuration": 10000,
-#         "dataset": [{"id": "dataset_raw", "source": raw_data}] + datasetWithFilters,
-#         "title": {"text": "Income in Europe since 1950"},
-#         "tooltip": {"order": "valueDesc", "trigger": "axis"},
-#         "xAxis": {"type": "category", "nameLocation": "middle"},
-#         "yAxis": {"name": "Income"},
-#         "grid": {"right": 140},
-#         "series": seriesList,
-#     }
-#     st_echarts(options=option, height="600px")
+    option = {
+        "animationDuration": 10000,
+        "dataset": [{"id": "dataset_raw", "source": raw_data}] + datasetWithFilters,
+        "title": {"text": "Income in Europe since 1950"},
+        "tooltip": {"order": "valueDesc", "trigger": "axis"},
+        "xAxis": {"type": "category", "nameLocation": "middle"},
+        "yAxis": {"name": "Income"},
+        "grid": {"right": 140},
+        "series": seriesList,
+    }
+    st_echarts(options=option, height="600px")
 
 
 ST_LINE_DEMOS = {
